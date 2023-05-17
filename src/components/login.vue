8448c005f9c098ff79298295f770c1ffd300d201
NsuMint <1569973720@qq.com>
2023-05-17 15:37:29 +0800

    login

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,23 +6,29 @@
     </div>
     <!-- 登录框 -->
     <div class="loginCard">
-      <span>登录</span>
+      <div style="font-size: 24px;line-height: 80px;left: 0;">登录</div>
       <el-form>
         <!-- 账号 -->
         <el-form-item>
-          <input type="text">
+          <span>账号</span>
+          <el-input type="text" class="input"></el-input>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item>
-          <input type="password">
+          <span>密码</span>
+          <el-input type="password" class="input"></el-input>
         </el-form-item>
-        <el-form-item>
+        <!-- 这里放formitem会多出一个格子有问题 -->
+        <!-- 我英语不好，类名将就着看吧 -->
+        <div class="remmberAndForget">
           <!-- 记住密码 -->
+          <el-checkbox>记住密码</el-checkbox>
           <!-- 忘记密码 -->
-        </el-form-item>
+          <div class="forget">忘记密码</div>
+        </div>
         <el-form-item>
           <!-- 登录按钮 -->
-          <el-button></el-button>
+          <el-button type="primary" style="width: 330px;">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -42,15 +48,7 @@ export default {
 }
 
 .title {
-  width: 652px;
-  height: 99px;
-  background-color: rgba(255, 255, 255, 0);
-  box-sizing: border-box;
-  font-family: "Arial Bold", "Arial Normal", "Arial", sans-serif;
-  font-weight: 700;
-  color: #333333;
   text-align: center;
-  line-height: normal;
 }
 .loginCard{
   background-color: #ffffff;
@@ -58,5 +56,48 @@ export default {
   height: 424px;
   border-radius: 10px;
   border: 1px solid #ffffff;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
 }
+.forget{
+  display: inline-block;
+  color: #1E98D7;
+  cursor:pointer
+}
+.forget:hover{
+  color:rgba(30, 152, 215, 0.804)
+}
+.remmberAndForget{
+  display: flex;
+  justify-content: space-between;
+}
+.input{
+  height: 30px;
+  width: 100%;
+}
+/*
+                    _ooOoo_
+                   o8888888o
+                   88" . "88
+                   (| -_- |)
+                   O\  =  /O
+                ____/`---'\____
+                 .' \\| |// `.
+               / \\||| : |||// \
+             / _||||| -:- |||||- \
+               | | \\\ - /// | |
+             | \_| ''\---/'' | |
+              \ .-\__ `-` ___/-. /
+           ___`. .' /--.--\ `. . __
+          ."" '< `.___\_<|>_/___.' >'"".
+         | | : `- \`.;`\ _ /`;./ -  | |
+         \ \ `-. \_ __\ /__ _/ .-` / /
+   ======`-.____`-.___\_____/___.-`____.-'======
+            `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+              佛祖保佑 永无BUG
+              
+*/
 </style>
--- src/main.js
+++ src/main.js
@@ -8,9 +8,28 @@ import './plugins/element.js'
 import './assets/css/global.css'
 //导入Fontawesome图标库
 import './fontawesome-free-6.3.0-web/css/all.min.css'
+//导入axios
+import axios from 'axios'
 // 设置请求的根路径
+// 此处填写服务器的根地址
+axios.defaults.baseURL = ''
 
 
+//token校验，请求拦截器
+//设置拦截器
+//use将会在向服务器发出请求时被调用
+axios.interceptors.request.use(
+  config => {
+    //Authoritarian就是我们需要的令牌，将token的值赋给它
+    //他就能让拦截器放行，从而获取表单数据
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    //在最后必须return config
+    return config
+  }
+  )
+
+//全局配置能够向AJAX发出请求
+Vue.prototype.$http = axios  
 Vue.config.productionTip = false
 
 new Vue({
